4a12b663500b991352e5ee1d15542cd66a232787
Manuel Adrian Chavez Sanchez <manuel.adrian.chavez@ibm.com>
2024-07-09 16:23:50 -0600

    spinner game

--- src/PhysicsScene.js
+++ src/PhysicsScene.js
@@ -1,48 +1,86 @@
 // https://pmndrs.github.io/react-three-rapier/
 
-import {
-  CapsuleCollider,
-  CuboidCollider,
-  Debug,
-  Physics,
-  RigidBody,
-} from "@react-three/rapier";
+import { useKeyboardControls } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { Debug, Physics, RigidBody } from "@react-three/rapier";
 import { useRef } from "react";
-
+import * as THREE from "three";
 const PhysicsScene = () => {
-  const firstCubeRef = useRef();
-  const secondCubeRef = useRef();
-  const firstCubeClickHandler = () => {
-    firstCubeRef.current.applyImpulse({ x: -100, y: 5, z: 0 });
+  const cubeRef = useRef();
+  const spinner = useRef();
+  const isJump = useRef(false);
+  const allKeys = useKeyboardControls((keys) => keys);
+  console.log(allKeys);
+
+  const cubeMovementHandler = () => {
+    if (allKeys.forward) {
+      cubeRef.current.applyImpulse({ x: 0, y: 0, z: -0.3 });
+    }
+    if (allKeys.backward) {
+      cubeRef.current.applyImpulse({ x: 0, y: 0, z: 0.3 });
+    }
+    if (allKeys.leftward) {
+      cubeRef.current.applyImpulse({ x: -0.3, y: 0, z: 0.3 });
+    }
+    if (allKeys.rightward) {
+      cubeRef.current.applyImpulse({ x: 0.3, y: 0, z: 0.3 });
+    }
+    if (isJump.current) {
+      if (allKeys.jump) {
+        cubeRef.current.applyImpulse({ x: 0, y: 40, z: 0.3 });
+        isJump.current = false;
+      }
+    }
+  };
+  useFrame((state) => {
+    const getElapsedTime = state.clock.getElapsedTime();
+    spinner.current.setNextKinematicTranslation({
+      x: 0,
+      y: Math.abs(Math.sin(getElapsedTime)),
+      z: 0,
+    });
+
+    const eulerRotationAngle = new THREE.Euler(0, getElapsedTime, 0);
+    const quaternionRotation = new THREE.Quaternion().setFromEuler(
+      eulerRotationAngle
+    );
+
+    spinner.current.setNextKinematicRotation(quaternionRotation);
+    cubeMovementHandler();
+  });
+  const cubeClickHandler = () => {
+    cubeRef.current.applyImpulse({ x: -25, y: 0, z: 0 });
   };
 
-  const secondCubeClickHandler = () => {
-    secondCubeRef.current.applyImpulse({ x: 100, y: 0, z: 0 });
-  };
   return (
     <Physics gravity={[0, -9.81, 0]}>
       <Debug />
-      <RigidBody ref={firstCubeRef} mass={20}>
-        <mesh
-          castShadow
-          position={[1.5, 2.5, 0]}
-          onClick={firstCubeClickHandler}
-          scale={3}
-        >
+      <RigidBody
+        ref={cubeRef}
+        position={[2.5, 2.5, 0]}
+        onCollisionEnter={() => (isJump.current = true)}
+        onCollisionExit={() => (isJump.current = false)}
+      >
+        <mesh castShadow onClick={cubeClickHandler}>
           <boxGeometry />
           <meshStandardMaterial color="#CC3941" />
         </mesh>
       </RigidBody>
-      <RigidBody ref={secondCubeRef} mass={200}>
-        <mesh position={[-1.5, 1.5, 0]} onClick={secondCubeClickHandler}>
-          <boxGeometry />
-          <meshBasicMaterial color="#CC3941" />
+      <RigidBody ref={spinner} position-y={-0.65} type="kinematicPosition">
+        <mesh receiveShadow>
+          <boxGeometry args={[1, 0.35, 15]} />
+          <meshBasicMaterial color="orange" />
         </mesh>
       </RigidBody>
 
-      <RigidBody type="fixed">
-        <mesh position-y={-1} rotation-x={-Math.PI * 0.5} receiveShadow>
-          <boxGeometry args={[8, 8, 0.35]} />
+      <RigidBody
+        type="fixed"
+        position-y={-1}
+        rotation-x={-Math.PI * 0.5}
+        restitution={0.5}
+      >
+        <mesh receiveShadow>
+          <boxGeometry args={[15, 15, 0.35]} />
           <meshStandardMaterial color="#C7CAC7" />
         </mesh>
       </RigidBody>
