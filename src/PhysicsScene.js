868091b4054fdfd4f51cfe1bb121efc19c901905
Manuel Adrian Chavez Sanchez <manuel.adrian.chavez@ibm.com>
2024-07-09 11:19:03 -0600

    adding example of coliders

--- src/PhysicsScene.js
+++ src/PhysicsScene.js
@@ -1,23 +1,38 @@
 // https://pmndrs.github.io/react-three-rapier/
 
-import { Physics, RigidBody } from "@react-three/rapier";
+import {
+  CapsuleCollider,
+  CuboidCollider,
+  Debug,
+  Physics,
+  RigidBody,
+} from "@react-three/rapier";
 
 const PhysicsScene = () => {
   return (
     <Physics gravity={[0, -9.81, 0]}>
-      <RigidBody>
-        <mesh castShadow position={[0, 1.5, 0]}>
+      <Debug />
+      <RigidBody position={[0, 1.5, 0]} colliders={false}>
+        <CuboidCollider args={[0.5, 0.5, 0.5]} />
+        {/* <CuboidCollider args={[0.25, 0.25, 0.25]} position={[-2, -1, -2.5]} /> */}
+        <mesh castShadow>
           <boxGeometry />
           <meshStandardMaterial color="#CC3941" />
         </mesh>
-        <mesh
-          castShadow
-          position={[0, 1.5, 0]}
-          scale={[0.25, 3, 1]}
-          position-z={-2}
-        >
-          <boxGeometry />
-          <meshStandardMaterial color="#CC3941" />
+      </RigidBody>
+      <RigidBody colliders="trimesh">
+        {/* <RigidBody colliders="hull"> */}
+        <mesh position={[-1.5, 1.5, 0]}>
+          <torusKnotGeometry args={[0.5, 0.15, 100, 100]} />
+          <meshBasicMaterial color="orange" />
+        </mesh>
+      </RigidBody>
+      {/* <RigidBody colliders="ball"> */}
+      <RigidBody colliders={false} position={[0, 1.5, -1.5]}>
+        <CapsuleCollider args={[0.375, 0.6]} />
+        <mesh>
+          <sphereGeometry args={[0.75, 64, 64]} />
+          <meshBasicMaterial color="purple" />
         </mesh>
       </RigidBody>
       <RigidBody type="fixed">
