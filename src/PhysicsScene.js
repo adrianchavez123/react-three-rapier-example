94c9b26c6839e1bcded4dc9ea667fcba5510b8e3
Manuel Adrian Chavez Sanchez <manuel.adrian.chavez@ibm.com>
2024-07-10 08:29:00 -0600

    cubes waterfall

--- src/PhysicsScene.js
+++ src/PhysicsScene.js
@@ -2,88 +2,66 @@
 
 import { useKeyboardControls } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
-import { Debug, Physics, RigidBody } from "@react-three/rapier";
-import { useRef } from "react";
+import {
+  Debug,
+  InstancedRigidBodies,
+  Physics,
+  RigidBody,
+} from "@react-three/rapier";
+import { useMemo, useRef } from "react";
 import * as THREE from "three";
 const PhysicsScene = () => {
-  const cubeRef = useRef();
-  const spinner = useRef();
-  const isJump = useRef(false);
-  const allKeys = useKeyboardControls((keys) => keys);
-  console.log(allKeys);
-
-  const cubeMovementHandler = () => {
-    if (allKeys.forward) {
-      cubeRef.current.applyImpulse({ x: 0, y: 0, z: -0.3 });
+  const count = 100;
+  const cubeTransformations = useMemo(() => {
+    const cubesPositions = [];
+    const cubesRotations = [];
+    const cubesScales = [];
+    for (let i = 0; i < count; i++) {
+      cubesPositions.push([
+        (Math.random() - 0.5) * 5,
+        Math.random() * 20,
+        (Math.random() - 0.5) * 5,
+      ]);
+      cubesRotations.push([0, 0, 0]);
+      cubesScales.push([0.2, 0.2, 0.2]);
     }
-    if (allKeys.backward) {
-      cubeRef.current.applyImpulse({ x: 0, y: 0, z: 0.3 });
-    }
-    if (allKeys.leftward) {
-      cubeRef.current.applyImpulse({ x: -0.3, y: 0, z: 0.3 });
-    }
-    if (allKeys.rightward) {
-      cubeRef.current.applyImpulse({ x: 0.3, y: 0, z: 0.3 });
-    }
-    if (isJump.current) {
-      if (allKeys.jump) {
-        cubeRef.current.applyImpulse({ x: 0, y: 40, z: 0.3 });
-        isJump.current = false;
-      }
-    }
-  };
-  useFrame((state) => {
-    const getElapsedTime = state.clock.getElapsedTime();
-    spinner.current.setNextKinematicTranslation({
-      x: 0,
-      y: Math.abs(Math.sin(getElapsedTime)),
-      z: 0,
-    });
-
-    const eulerRotationAngle = new THREE.Euler(0, getElapsedTime, 0);
-    const quaternionRotation = new THREE.Quaternion().setFromEuler(
-      eulerRotationAngle
-    );
-
-    spinner.current.setNextKinematicRotation(quaternionRotation);
-    cubeMovementHandler();
-  });
-  const cubeClickHandler = () => {
-    cubeRef.current.applyImpulse({ x: -25, y: 0, z: 0 });
-  };
-
+    return {
+      positions: cubesPositions,
+      rotations: cubesRotations,
+      scales: cubesScales,
+    };
+  }, []);
   return (
     <Physics gravity={[0, -9.81, 0]}>
       <Debug />
-      <RigidBody
-        ref={cubeRef}
-        position={[2.5, 2.5, 0]}
-        onCollisionEnter={() => (isJump.current = true)}
-        onCollisionExit={() => (isJump.current = false)}
-      >
-        <mesh castShadow onClick={cubeClickHandler}>
+      <RigidBody>
+        <mesh castShadow position={[0, 1.5, 0]}>
           <boxGeometry />
           <meshStandardMaterial color="#CC3941" />
         </mesh>
       </RigidBody>
-      <RigidBody ref={spinner} position-y={-0.65} type="kinematicPosition">
-        <mesh receiveShadow>
-          <boxGeometry args={[1, 0.35, 15]} />
-          <meshBasicMaterial color="orange" />
-        </mesh>
-      </RigidBody>
 
-      <RigidBody
-        type="fixed"
-        position-y={-1}
-        rotation-x={-Math.PI * 0.5}
-        restitution={0.5}
-      >
-        <mesh receiveShadow>
-          <boxGeometry args={[15, 15, 0.35]} />
+      <RigidBody type="fixed">
+        <mesh
+          position-y={-1}
+          rotation-x={-Math.PI * 0.5}
+          receiveShadow
+          geometry={new THREE.BoxGeometry(8, 8, 0.35)}
+        >
+          {/* <boxGeometry args={[8, 8, 0.35]} /> */}
           <meshStandardMaterial color="#C7CAC7" />
         </mesh>
       </RigidBody>
+      <InstancedRigidBodies
+        positions={cubeTransformations.positions}
+        rotations={cubeTransformations.rotations}
+        scale={cubeTransformations.scales}
+      >
+        <instancedMesh args={[null, null, count]} castShadow>
+          <boxGeometry />
+          <meshBasicMaterial color="#CC3841" />
+        </instancedMesh>
+      </InstancedRigidBodies>
     </Physics>
   );
 };
